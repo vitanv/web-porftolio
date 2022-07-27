be45fd1d2a87dc89e11005bcb0828028e9f41e82
vitanv <vitanvlad20@gmail.com>
2022-07-27 22:26:14 +0100

    Changed navbar to respond to scroll

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+var nav = document.getElementById("navigation")
+window.onscroll = function (){
+    "use strict";
+    if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200){
+        nav.classList.add("nav-visible");
+        nav.classList.remove("nav-invisible");
+    }else{
+        nav.classList.add("nav-invisible");
+        nav.classList.remove("nav-visible");
+    }
+}
