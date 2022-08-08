9e708f61944f9364ffc9bf6a893d529b0bc80348
vitanv <vitanvlad20@gmail.com>
2022-08-08 21:53:33 +0100

    initialize express

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
+
 require("dotenv").config();
+
+const app = express();
+
+app.route("/").get(function(req, res){
+    res.sendFile(process.cwd() + "index.html")
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log('${PORT}');
+});
